0221fc7cfd5f840e853ff516b35a2cf3f640172a
Nikita <nick1tapinyagin@yandex.ru>
2025-01-16 04:10:47 +0300

    edit the image on the place page

--- places/admin.py
+++ places/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from .models import Place, Image
 
 # Register your models here.
-admin.site.register(Place)
 admin.site.register(Image)
+
+class ImageInline(admin.TabularInline):
+    model = Image
+
+@admin.register(Place)
+class ImageAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInline,
+    ]
