51dbfe615b60383b9bff069eb31e4a4621279eda
Nikita <nick1tapinyagin@yandex.ru>
2025-03-01 02:59:07 +0300

    Update urls.py

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -8,6 +8,6 @@ from places import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.show_startpage),
-    path('places/<int:place_id>', views.show_detail_place),
+    path('places/<int:place_id>/', views.show_detail_place),
     path('tinymce/', include('tinymce.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
