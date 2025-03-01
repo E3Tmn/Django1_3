87c506e4d23dfc45ccb796956599b275f4a5a7a6
Nikita <nick1tapinyagin@yandex.ru>
2025-03-01 03:33:58 +0300

    update models.py

--- places/models.py
+++ places/models.py
@@ -4,9 +4,11 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField('Название', max_length=200)
-    short_description = models.TextField('Краткое описание')
-    long_description = HTMLField('Описание')
-    longitude = models.DecimalField('Долгота', max_digits=17, decimal_places=14)
+    short_description = models.TextField('Краткое описание', blank=True)
+    long_description = HTMLField('Описание', blank=True)
+    longitude = models.DecimalField('Долгота',
+                                    max_digits=17,
+                                    decimal_places=14)
     latitude = models.DecimalField('Широта', max_digits=17, decimal_places=14)
 
     def __str__(self):
@@ -19,7 +21,7 @@ class Image(models.Model):
                               verbose_name='Место',
                               related_name='images')
     image = models.ImageField('Фото')
-    order = models.IntegerField('Номер')
+    order = models.IntegerField('Номер', default=0)
 
     def __str__(self):
         return f'{self.id} {self.place.title}'
