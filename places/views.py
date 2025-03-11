dcfd55cf81f1c2165007d75f90689342a10b1bd8
Nikita <nick1tapinyagin@yandex.ru>
2025-03-12 02:53:28 +0300

    Update views.py

--- places/views.py
+++ places/views.py
@@ -5,7 +5,7 @@ from .models import Place, Image
 
 
 def show_detail_place(request, place_id):
-    place = get_object_or_404(Place.objects.select_related(), id=place_id)
+    place = get_object_or_404(Place.objects.prefetch_related('images'), id=place_id)
     serialize_place = JsonResponse(
         {
             'title': place.title,
